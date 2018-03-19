2e636ad6c12b30d8dc5c0f44dc35c7e8519f2578
SaeWooKR <36213940+SaeWooKR@users.noreply.github.com>
2018-03-20 00:06:53 +0900

    검색 수정

--- js/super-search.js
+++ js/super-search.js
@@ -125,8 +125,8 @@
             searchResultsEl.innerHTML = matchingPosts.map(function (post) {
                 d = new Date(post.pubDate);
 				var tmp1;
-				if(post.title.length>12){
-					tmp1 = post.title.substr(0,12);
+				if(post.title.length>11{
+					tmp1 = post.title.substr(0,11);
 					var tmp2 = post.title.substr(post.title.length-3,post.title.length-1);
 					tmp1 = tmp1+'...'+tmp2;
 				}else{
